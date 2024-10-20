6fb7d158a9b9e1dd433307c23b809bb3c9594707
odu1ros <xrosnex@mail.ru>
2024-10-20 22:20:44 +0000

    assignment 9

--- hw1/assignment_13.sh
+++ hw1/assignment_13.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
 words_file="ohio_plants_hangman.txt"
-# shuffle and select first word
-word=$(shuf -n 1 "$words_file")
-
-# an array of underscores
-masked_word=""
-for ((i=0; i<${#word}; i++)); do 
-	masked_word+="_"
-done
-
-# current errors count, max errors label, incorrect letters array
-errors=0
-max_errors=6
-incorrect_letters=()
 
 # printing out current game state: guessed letters, incorrect letters, asking for a new input
 function echo_game_state {
 	echo "$masked_word"
 	echo "Errors ($errors): ${incorrect_letters[*]}"
 	echo -n "Input a letter: "
+}
+
+# letting user choose if he wants to play the next game (generate a word if yes, exit if no)
+function start_new_game {
+	read -n1 -p "Start a new game? [Y/n] " state
+	echo
+	
+	if [[ "$state" == "Y" || "$state" == "y" ]]; then
+		# shuffle textfile and select first word
+		word=$(shuf -n 1 "$words_file")
+
+		# an array of underscores
+		masked_word=""
+		for ((i=0; i<${#word}; i++)); do 
+			masked_word+="_"
+		done
+		
+		# current errors count, max errors label, incorrect letters array
+		errors=0
+		max_errors=6
+		incorrect_letters=()
+	else
+		exit 1
+	fi
 }
 
 # update masked word if the guessed letter is correct
@@ -40,37 +51,40 @@ function update_masked_word {
 	masked_word=$new_masked_word
 }
 
-# until the word is guessed or errors number < 6 play a game
-while [[ "${masked_word[*]}" != "$word" && $errors -lt $max_errors ]]; do
-	echo_game_state
-	read -n1 input
-	echo
-
-	# check if user input is correct:
-	# 1) it should be a letter a-z any case
-	# 2) the letter should not be guessed already (either be anywhere within masked_word or incorrect_letters)
-	while [[ ! "$input" =~ [a-zA-Z] || "${masked_word}" == *"$input"* || "${incorrect_letters[*]}" == *"$input"* ]]; do
-		read -n1 -p "Input a letter: " input
+while true; do
+	start_new_game
+	# until the word is guessed or errors number < 6 play a game
+	while [[ "${masked_word[*]}" != "$word" && $errors -lt $max_errors ]]; do
+		echo_game_state
+		read -n1 input
 		echo
-	done
+
+		# check if user input is correct:
+		# 1) it should be a letter a-z any case
+		# 2) the letter should not be guessed already (either be anywhere within masked_word or incorrect_letters)
+		while [[ ! "$input" =~ [a-zA-Z] || "${masked_word}" == *"$input"* || "${incorrect_letters[*]}" == *"$input"* ]]; do
+			read -n1 -p "Input a letter: " input
+			echo
+		done
   	
-  	# convert input to lowercase if needed
-	input=$(echo "$input" | tr '[:upper:]' '[:lower:]')
+  		# convert input to lowercase if needed
+		input=$(echo "$input" | tr '[:upper:]' '[:lower:]')
   
-	# now try finding input inside word:
-	# 1) if it is present, update the mask
-	# 2) if it is not, count+ errors and add it to incorrect letters array
-	if [[ "$word" == *"$input"* ]]; then
-		update_masked_word "$input"
+		# now try finding input inside word:
+		# 1) if it is present, update the mask
+		# 2) if it is not, count+ errors and add it to incorrect letters array
+		if [[ "$word" == *"$input"* ]]; then
+			update_masked_word "$input"
+		else
+			incorrect_letters+=("$input")
+			((errors++))
+		fi
+	done
+
+	# now the result
+	if [[ "$masked_word" == "$word" ]]; then
+		echo "Congratulations! You guessed the word: $word"
 	else
-		incorrect_letters+=("$input")
-		((errors++))
+		echo "Game over! The correct word was: $word"
 	fi
 done
-
-# now the result
-if [[ "$masked_word" == "$word" ]]; then
-	echo "Congratulations! You guessed the word: $word"
-else
-	echo "Game over! The correct word was: $word"
-fi
